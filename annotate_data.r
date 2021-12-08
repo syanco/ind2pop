2f689d447de38d578221d71e5d4f81627192614c
syanco <syanco@gmail.com>
2021-12-08 13:45:28 -0500

    progress messages

--- annotate_data.r
+++ annotate_data.r
@@ -21,6 +21,8 @@ source("src/funs/big_stoat.r")
 
 #-- Individual Data 
 
+message("Loading movement data...")
+
 # load Kruger Elephant data from file, modify vars to work with STOAT
 elephants <- read.csv("data/African elephants in Etosha National Park (data from Tsalyuk et al. 2018).csv") %>% 
   rename(lng = location.long,
@@ -35,6 +37,7 @@ storks <- read.csv("data/MPIO white stork lifetime tracking data (2013-2014)-gps
 
 #-- GBIF/MOL Data 
 
+message("Loading MOL data...")
 # pull pre-annotations from MOL, filter to movement bounding box + 1 degree
 elephant_mol <- get_species_scenario('Loxodonta africana', 'landsat8', 'evi', 100, 16) %>% 
   # filter(latitude >= (bb_ele$minlat-1) & latitude <= (bb_ele$maxlat+1),
@@ -97,6 +100,7 @@ vars <- list(list(id = "MODIS/MCD43A4_006_NDVI",
 #-- Movement Data
 
 #- Elephants
+message("Annotating elephant movements...")
 elephants_anno <- big_stoat(data = elephants, vars = vars)
 
 # write to file to avoid re-annotating
@@ -104,6 +108,7 @@ write.csv(elephants_anno, file = "data/elephants_annotated.csv")
 
 
 # Storks
+message("Annotating stork movements...")
 storks_anno <- big_stoat(data = storks, vars = vars)
 
 # write to file to avoid re-annotating
@@ -113,14 +118,14 @@ write.csv(storks_anno, file = "data/storks_annotated.csv")
 #-- MOL data
 
 #- Elephants
-
+message("Annotating elephants from MOL...")
 elephants_mol_anno <- big_stoat(data = elephant_mol, vars =  vars)
 
 # write to file to avoid re-annotating
 write.csv(elephants_mol_anno, file = "data/elephants_mol_annotated.csv")
 
 #- Storks
-
+message("Annotating storks from MOL...")
 stork_mol_anno <- big_stoat(data = stork_mol, vars =  vars)
 
 # write to file to avoid re-annotating
